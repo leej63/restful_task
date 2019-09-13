aa497fad36de9266a0249d98a7133e33d170a1a9
Jonathan Lee <jonathan_496@hotmail.com>
2019-09-12 18:41:55 -0700

    first commit

---
+++ public/src/app/http.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  getTasks(){
+    // our http response is an Observable, store it in a variable
+    let tempObservable = this._http.get('/tasks');
+    // subscribe to the Observable and provide the code we would like to do with our data from the response
+    tempObservable.subscribe(data => console.log("Got our tasks!", data));
+ }
+
+  // getOneTask(id){
+  //   let tempObservable = this._http.get('/tasks/' + id);
+  //   tempObservable.subscribe(data => console.log("Got specified task!", data));
+  // }
+
+  constructor(private _http: HttpClient) {
+    this.getTasks();
+    // this.getOneTask();
+  }
+}
